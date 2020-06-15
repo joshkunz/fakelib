f680c3700ab259c18d5c3806eeb5a58ad16e92c0
Josh Kunz <josh@kunz.xyz>
2020-06-14 17:19:00 -0700

    Split 'filesystem' component into separate module.

--- fakelib.go
+++ fakelib.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"context"
 	"flag"
+	"fmt"
 	"log"
 	"os"
-	"path"
-	"strings"
-	"syscall"
 
-	"github.com/hanwen/go-fuse/v2/fs"
-	"github.com/hanwen/go-fuse/v2/fuse"
-
+	"fakelib/filesystem"
 	"fakelib/library"
 )
 
@@ -22,74 +17,14 @@ var (
 	albumsPerArtist = flag.Int("albums_per_artist", 3, "Max number of albums for each artist")
 )
 
-type songInode struct {
-	fs.Inode
-
-	song library.Song
-}
-
-var _ fs.NodeOpener = (*songInode)(nil)
-var _ fs.NodeReader = (*songInode)(nil)
-var _ fs.NodeGetattrer = (*songInode)(nil)
-
-func (s *songInode) Open(context.Context, uint32) (fs.FileHandle, uint32, syscall.Errno) {
-	return nil, 0, fs.OK
-}
-
-func (s *songInode) Read(_ context.Context, _ fs.FileHandle, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
-	s.song.Read(dest, off)
-	return fuse.ReadResultData(dest), fs.OK
-}
-
-func (s *songInode) Getattr(_ context.Context, _ fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
-	out.Size = uint64(s.song.Size())
-	return fs.OK
-}
-
-type root struct {
-	fs.Inode
-
-	l *library.Library
-}
-
-var _ fs.NodeOnAdder = (*root)(nil)
-
-func (r *root) OnAdd(ctx context.Context) {
-	for i := 0; i < r.l.Tracks; i++ {
-		location, err := r.l.PathAt(i)
-		if err != nil {
-			log.Fatalf("failed to get path at idx %d: %v", i, err)
-		}
-		song, err := r.l.SongAt(i)
-		if err != nil {
-			log.Fatalf("failed to get song at idx %d: %v", i, err)
-		}
-		dir, fname := path.Split(location)
-
-		wd := &r.Inode
-		for _, component := range strings.Split(dir, "/") {
-			if component == "" {
-				continue
-			}
-
-			cur := wd.GetChild(component)
-			if cur == nil {
-				cur = wd.NewPersistentInode(ctx, &fs.Inode{}, fs.StableAttr{Mode: fuse.S_IFDIR})
-				wd.AddChild(component, cur, true)
-			}
-
-			wd = cur
-		}
-
-		node := wd.NewPersistentInode(ctx, &songInode{song: song}, fs.StableAttr{})
-		wd.AddChild(fname, node, true)
-	}
-}
-
 func main() {
 	flag.Parse()
 	if len(flag.Args()) < 2 {
 		log.Fatalf("usage: %s golden.mp3 mount/", os.Args[0])
+	}
+
+	if *minPathLength < 3 {
+		log.Fatalf("--min_path_length must be at least 3")
 	}
 
 	goldenPath, mountDir := flag.Arg(0), flag.Arg(1)
@@ -111,20 +46,17 @@ func main() {
 	// No need for the file anymore, just close it to drop the handle.
 	golden.Close()
 
-	if *minPathLength < 3 {
-		log.Fatalf("--min_path_length must be at least 3")
-	}
-
 	if _, err := os.Stat(mountDir); os.IsNotExist(err) {
 		os.Mkdir(mountDir, 0755)
 	} else if err != nil {
 		log.Fatalf("failed to stat %q: %v", mountDir, err)
 	}
 
-	server, err := fs.Mount(mountDir, &root{l: lib}, nil)
+	server, err := filesystem.Mount(lib, mountDir, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Printf("filesystem mounted at %q\n", mountDir)
+	fmt.Printf("unmount with `fusermount -u %s`\n", mountDir)
 	server.Serve()
-	server.Wait()
 }
---
+++ filesystem/filesystem.go
@@ -0,0 +1,87 @@
+package filesystem
+
+import (
+	"context"
+	"log"
+	"path"
+	"strings"
+	"syscall"
+
+	"github.com/hanwen/go-fuse/v2/fs"
+	"github.com/hanwen/go-fuse/v2/fuse"
+
+	"fakelib/library"
+)
+
+type song struct {
+	fs.Inode
+
+	song library.Song
+}
+
+var _ fs.NodeOpener = (*song)(nil)
+var _ fs.NodeReader = (*song)(nil)
+var _ fs.NodeGetattrer = (*song)(nil)
+
+func (s *song) Open(context.Context, uint32) (fs.FileHandle, uint32, syscall.Errno) {
+	return nil, 0, fs.OK
+}
+
+func (s *song) Read(_ context.Context, _ fs.FileHandle, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
+	s.song.Read(dest, off)
+	return fuse.ReadResultData(dest), fs.OK
+}
+
+func (s *song) Getattr(_ context.Context, _ fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
+	out.Size = uint64(s.song.Size())
+	return fs.OK
+}
+
+type root struct {
+	fs.Inode
+
+	l *library.Library
+}
+
+var _ fs.NodeOnAdder = (*root)(nil)
+
+func (r *root) OnAdd(ctx context.Context) {
+	for i := 0; i < r.l.Tracks; i++ {
+		location, err := r.l.PathAt(i)
+		if err != nil {
+			log.Fatalf("failed to get path at idx %d: %v", i, err)
+		}
+		lSong, err := r.l.SongAt(i)
+		if err != nil {
+			log.Fatalf("failed to get song at idx %d: %v", i, err)
+		}
+		dir, fname := path.Split(location)
+
+		wd := &r.Inode
+		for _, component := range strings.Split(dir, "/") {
+			if component == "" {
+				// `dir` likely has a trailing `/` which yields an empty path
+				// component on split, so ignore that component.
+				continue
+			}
+
+			cur := wd.GetChild(component)
+			if cur == nil {
+				cur = wd.NewPersistentInode(ctx, &fs.Inode{}, fs.StableAttr{Mode: fuse.S_IFDIR})
+				wd.AddChild(component, cur, true)
+			}
+
+			wd = cur
+		}
+
+		node := wd.NewPersistentInode(ctx, &song{song: lSong}, fs.StableAttr{})
+		wd.AddChild(fname, node, true)
+	}
+}
+
+// Mount mounts the given library into `dir`. `options` can be used to supply
+// additional FUSE mount options. If the default options are OK, then `nil`
+// can safely be provided for `options`.
+func Mount(lib *library.Library, dir string, options *fs.Options) (*fuse.Server, error) {
+	return fs.Mount(dir, &root{l: lib}, options)
+}
